5fade27076fb50d5ea05644288a5cd71b9a65a27
Charlean Baxter <baxtercm@outlook.com>
2022-12-29 01:50:40 -0800

    winning message

--- src/App.js
+++ src/App.js
@@ -7,6 +7,29 @@ const App = () => {
   const [squares, setSquares] = useState(Array(9).fill(null))
   // first player
   const [opponent, setOpponent] = useState("❎")
+  const [winner, setWinner] = useState("")
+  const winning = () => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ]
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] === "❎" && squares[a] === squares[b] && squares[a] === squares[c]) {
+        setWinner("❎")
+      } else if (squares[a] === "🅾️" && squares[a] === squares[b] && squares[a] === squares[c]) {
+        setWinner("🅾️")
+      }
+    }
+    return null;
+  }
+
   const switchOpponent = (player) => {
     // conditional to prevent multiple entries on same box
     if(squares[player] === null) {
@@ -16,11 +39,14 @@ const App = () => {
       updateSquare[player] = opponent
       setSquares(updateSquare)
     }
+    winning()
   }
+
 
   return (
     <>
       <h1>Tic Tac Toe</h1>
+      {winner && <h2>{winner} wins!</h2>}
       <div className="grid">
         {squares.map((value, index) => 
           <div key={index}>
